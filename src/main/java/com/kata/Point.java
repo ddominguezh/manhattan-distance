c3757606f19353240fd615577576eadb77f30477
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-06-27 21:17:05 +0200

    distance between two points

--- src/main/java/com/kata/Point.java
+++ src/main/java/com/kata/Point.java
@@ -2,4 +2,16 @@ package com.kata;
 
 public class Point {
     
+    private int x;
+    private int y;
+    protected Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public static Point create(int x, int y){
+        return new Point(x, y);
+    }
+    public int distance(Point point){
+        return (int) Math.abs(x - point.x) + Math.abs(y - point.y);
+    }
 }
--- src/test/java/com/kata/PointTest.java
+++ src/test/java/com/kata/PointTest.java
@@ -1,5 +1,16 @@
 package com.kata;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class PointTest {
     
+    @Test
+    public void calculate_distance(){
+        assertEquals(0, Point.create(1, 1).distance(Point.create(1, 1)));
+        assertEquals(4, Point.create(5, 4).distance(Point.create(3, 2)));
+        assertEquals(4, Point.create(3, 2).distance(Point.create(5, 4)));
+        assertEquals(3, Point.create(1, 1).distance(Point.create(0, 3)));
+    }  
 }
